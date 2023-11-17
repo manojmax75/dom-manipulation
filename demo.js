5e6d10cdbf1e8007be24f7a5acf4f5d313f385e8
manojmax75 <manojmax75@gmail.com>
2023-11-17 23:13:14 +0530

    edit_add_fucntionality

--- demo.js
+++ demo.js
@@ -1,167 +1,43 @@
-// console.log(document.location)
-// console.log(document.URL)
-// console.log(document.title)
-// document.title=123;
-// console.log(document.title)
-// console.log(document.doctype)
-// console.log(document)
-// console.log(document.head)
+var form=document.getElementById("addForm")
+var itemList=document.getElementById("items")
 
-// console.log(document.all)
-// console.log(document.forms[0])
+form.addEventListener("submit",addItem);
+itemList.addEventListener('click', removeitem);
 
-// console.log(document.links)
-// console.log(document.images)
+function addItem(e){
+    e.preventDefault();
+    var newitem=document.getElementById('item').value
 
-// const title=document.getElementById("header-title")
-// const mainheader=document.getElementById("main-header")
+    var li=document.createElement("li")
+   
+    li.className="list-group-item"
+    console.log(li)
+    li.appendChild(document.createTextNode(newitem))
 
-// console.log(title)
-// title.textContent="goodbye"
-// title.textContent="hello"
+    var button=document.createElement("button")
+    button.className="btn btn-danger btn-sm float-right delete"
+    button.appendChild(document.createTextNode("X"))
+    li.appendChild(button)
+    var editbutton=document.createElement("button")
+    editbutton.className="btn btn-primary btn-sm float-right edit"
+    editbutton.appendChild(document.createTextNode("edit"))
+    li.appendChild(editbutton)
 
-// console.log(title.innerText)
-// console.log(title.textContent)
-
-
-
-// console.log(title.innerHTML="<h3>hello</h3>")
-
-// title.style.borderBottom="solid 1px #000"
-// mainheader.style.borderBottom="solid 3px #000"
+    itemList.append(li)
 
 
-// edit the li by class name 
+}
 
+function removeitem(e){
+   
 
-// var items= document.getElementsByClassName("list-group-item");
-// console.log(items[1])
-// items[2].style.color="green"
-// // items[1].style.backgroundColor="green"
+    if(e.target.classList.contains('delete')){
+        if(confirm("are you sure?")){
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
+    
 
-// for(let i=0;i<items.length;i++){
-//     items[i].style.fontWeight="bold"
-// }
-
-//edit the li by tag name
-
-// var li= document.getElementsByTagName("li");
-// console.log(li[1])
-// li[2].style.color="green"
-// li[1].style.backgroundColor="#f4f4f4"
-
-// for(let i=0;i<li.length;i++){
-//     li[i].style.fontWeight="bold"
-//     li[i].style.backgroundColor="#f4f4f4"
-// }
-
-
-//QUERY SELECTOR 
-
-// var header=document.querySelector("#main-header")
-// header.style.borderBottom="solid 5px red" 
-
-// var input=document.querySelector("input")
-// input.value="hello world"
-
-// var submit=document.querySelector('input[type="submit"]')
-// submit.value="send"
-
-// var si=document.querySelector('.list-group-item:nth-child(1)')
-// si.style.color="red"
-
-
-
-
-
-
-
-
-
-// const item=document.querySelectorAll("li")
-// console.log(item)
-// item[1].style.backgroundColor="green"
-// item[2].style.display="none"
-// for(let i=0;i<item.length;i++){
-//    if(i%2===0){
-//     item[i].style.fontWeight="bold"
-//    }
-// }
-
-
-// TRAVERSING THE DOM 
-
-var itemslist=document.querySelector("#items");
-// console.log(itemslist.parentNode)
-// itemslist.parentNode.style.backgroundColor="#f4f4f4"
-
-
-// var itemslist=document.querySelector("#items");
-// console.log(itemslist.parentElement)
-// itemslist.parentElement.style.backgroundColor="#f4f4f4"
-
-// child nodes
-console.log(itemslist.childNodes)
-console.log(itemslist.children)
-console.log(itemslist.children[1])
-itemslist.children[1].style.color="red"
-
-// first child
-
-console.log(itemslist.firstChild)
-// first element child 
-console.log(itemslist.firstElementChild)
-itemslist.firstElementChild.style.color="blue"
-
-
-// last child
-
-console.log(itemslist.lastChild)
-// last element child 
-console.log(itemslist.lastElementChild)
-itemslist.lastElementChild.style.color="blue"
-
-// nextSibling
-
-console.log(itemslist.nextSibling)
-//  nextElementSibling
-console.log(itemslist.nextElementSibling)
-itemslist.nextElementSibling.style.color="red"
-
-
-// previousSibling
-
-console.log(itemslist.prevuousSibling)
-// previousElementSibling
-console.log(itemslist.prevuousElementSibling)
-itemslist.previousElementSibling.style.color="red"
-
-//create element 
-
-var newdiv=document.createElement("div");
-newdiv.className="hello"
-newdiv.id="hello1"
-newdiv.setAttribute("title","hello div")
-console.log(newdiv)
-
-// create a text Node
-
-var textnode=document.createTextNode("Hello world")
-newdiv.appendChild(textnode)
-
-// insert in dom
-var insertdom=document.querySelector("header .container")
-var h1=document.querySelector("header h1")
-
-insertdom.insertBefore(newdiv,h1)
-
-var headertitle=document.querySelector("#header-title")
-
-var newspan=document.createElement("span")
-newspan.textContent="HEllo "
-headertitle.insertBefore(newspan,headertitle.firstChild)
-
-var listitem=document.querySelector(".list-group-item:first-child")
-var newspans=document.createElement("span")
-newspans.textContent="HEllo "
-listitem.insertBefore(newspans,listitem.firstChild)
+    
+}
