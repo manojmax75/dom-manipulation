5da404dcb8f3bd630bb3e6127ae0f56dd66fd9a3
manojmax75 <manojmax75@gmail.com>
2023-11-18 16:56:20 +0530

    multiple_user

--- demo.js
+++ demo.js
@@ -2,12 +2,13 @@ const myform=document.querySelector("#my-form")
 var name1=document.querySelector("#name")
 var email1=document.querySelector("#email")
 const msg=document.querySelector(".msg")
+var number=document.querySelector("#phonenumber")
 
 myform.addEventListener("submit",localstore)
 
 function localstore(e){
     e.preventDefault();
-    if(name1.value==="" || email1.value===""){
+    if(name1.value==="" || email1.value===""|| number.value===""){
         msg.classList.add('error')
         msg.innerHTML="please enter the feilds"
         setTimeout(()=>{
@@ -16,10 +17,11 @@ function localstore(e){
     }else{
         let myobj={
             name:name1.value,
-            email1:email1.value
+            email:email1.value,
+            phone_number:number.value
         }
         myobj_serialize=JSON.stringify(myobj);
-        localStorage.setItem("myuser",myobj_serialize)
+        localStorage.setItem(email1.value,myobj_serialize)
 
         console.log(JSON.parse(localStorage.getItem("myuser")))
        
